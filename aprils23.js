6c87ebccc6dac680c6d90b2b1e3f6f38376fe5bd
sksutkar <swapnilsutkar7123@gmail.com>
2026-04-23 11:45:16 +0530

    aprils23.js updated file

--- aprils23.js
+++ aprils23.js
@@ -11,10 +11,26 @@ while (num > 0) {
     num = Math.floor(num / 10);
 }
 
-console.log("The Reverse Number :",res);
+console.log("The Reverse Number :", res);
 
 //Reverse In String
 
-const  ResStr=StrName=>[...StrName].reverse().join('');
+const ResStr = StrName => [...StrName].reverse().join('');
 console.log(ResStr('swapnil'));
 
+
+let ResNumbar = [1, 25, 64, 8, 23, 85];
+ResNumbar.reverse();
+console.log("Reversed Number = ", ResNumbar);
+
+//Sum of Digits of a Number
+
+let SumNum = 7128;
+let SumN = 0;
+
+while (SumNum > 0) {
+    SumN += SumNum % 10;
+    SumNum = Math.floor(SumNum / 10);
+}
+
+console.log("The Sum of Digits = ", SumN);
